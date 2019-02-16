2ca2b35f5e96215a09eb8262e0ac3cc8a053ca11
Aravind RaJ <aravinu19@gmail.com>
2019-02-16 07:34:45 +0530

    initial stage: started

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyparser = require("body-parser");
+var Proxify = require("./app.js");
+
+var app = express();
+app.set('port', (process.env.PORT || 8080));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+Proxify(app);
+
+var server = app.listen(app.get('port'), () => {
+  console.log("Proxify Server is Up and Running on ", server.address().port);
+});
